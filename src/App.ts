869b30009e0bc31d866e74eaf5276caba5b5e0c1
David Sandoval <ing.david1993@gmail.com>
2023-01-31 15:20:40 -0500

    build: :tada: Initialize project

---
+++ src/App.ts
@@ -0,0 +1,10 @@
+import "./index.css";
+
+document.getElementById("app").innerHTML = `
+<div class="container">
+  <div>Name: demo-microfrontend-core</div>
+  <div>Framework: vanilla</div>
+  <div>Language: TypeScript</div>
+  <div>CSS: Empty CSS</div>
+</div>
+`;
